461ee69ed8c7b1a88c2bf20b2a151553c3fc82f4
BestBall <thanakorn.boonlar@gmail.com>
2023-01-10 02:19:57 +0700

    feat:add feature responsive home page

--- client/src/components/Homepage/Profile.jsx
+++ client/src/components/Homepage/Profile.jsx
@@ -1,64 +1,51 @@
 import React from "react";
-import { Typography, Link, Box } from "@mui/material";
+import { Typography, Link, Box, Stack } from "@mui/material";
 
 const Profile = (props) => {
   const { name, github, linkedin, img } = props;
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <img src={img} alt={name} />
-      </Box>
-      <Typography
-        marginTop="16px"
-        variant="meetTeam"
-        component="p"
-        color="#000"
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        {name}
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "18px",
-          gap: "53px",
-        }}
-      >
+    <>
+      <Stack direction={"column"} justifyContent="center" alignItems={"center"}>
+        <Box>
+          <img src={img} alt={name} />
+        </Box>
+        <Typography
+          marginTop="16px"
+          variant="meetTeam"
+          color="#000"
+          textAlign={"center"}
+        >
+          {name}
+        </Typography>
         <Box
           sx={{
             display: "flex",
+            justifyContent: "center",
+            marginTop: "18px",
+            gap: "53px",
           }}
         >
-          <Link href={github}>
-            <img src="images/github.svg" alt="github icon" />
-          </Link>
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Link href={github}>
+              <img src="images/github.svg" alt="github icon" />
+            </Link>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Link href={linkedin}>
+              <img src="images/linkedin.svg" alt="linkedin icon" />
+            </Link>
+          </Box>
         </Box>
-        <Box
-          sx={{
-            display: "flex",
-          }}
-        >
-          <Link href={linkedin}>
-            <img src="images/linkedin.svg" alt="linkedin icon" />
-          </Link>
-        </Box>
-      </Box>
-    </Box>
+      </Stack>
+    </>
   );
 };
 
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import NavBox from "../components/Navbar.jsx";
 import Profile from "../components/Homepage/Profile.jsx";
-import { Typography, Button, Divider, Box } from "@mui/material";
+import { Typography, Button, Divider, Box, Hidden } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { Stack } from "@mui/system";
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -41,95 +42,89 @@ const Homepage = () => {
   ];
   return (
     <>
-      <Box>
+      <Box id="Nav-bar">
         <NavBox />
       </Box>
-      <Box width="auto">
-        <Box
-          margin="0px"
-          marginTop="64px"
-          width="auto"
-          height="897px"
-          sx={{
-            display: "flex",
-            backgroundColor: "#F5F5F6",
-            justifyContent: "center",
-          }}
+
+      <Box width="100vw" id="Content-container">
+        {/* --------------------------------------- Create account Section --------------------------------------- */}
+        <Stack
+          id="Create-account-section"
+          fullWidth
+          height="auto"
+          marginTop={{ xs: 12, sm: 15 }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              width: "auto",
-              flexDirection: "column",
-              justifyContent: "flex-start",
-              alignContent: "center",
-              paddingTop: "32px",
-            }}
+          <Stack
+            id="Title-content"
+            direction={"column"}
+            justifyContent="center"
+            alignItems={"center"}
           >
-            <Box>
-              <Typography
-                variant="h2"
-                component="h1"
-                color="warning"
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                The place where
-              </Typography>
-              <Typography
-                variant="h2"
-                component="h1"
-                color="warning"
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                you get &nbsp;
-                <Typography
-                  variant="h2"
-                  component="span"
-                  color="primary"
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                  }}
-                >
-                  that
-                </Typography>
-                &nbsp; job
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                display: "flex",
-                width: "auto",
-                justifyContent: "center",
-                paddingTop: "32px",
+            <Stack
+              direction={{
+                xs: "column",
+                sm: "row",
               }}
+              spacing={{ xs: 1, sm: 2, md: 4 }}
+              justifyContent="center"
+              alignItems={"center"}
+              // marginTop={{ xs: 0, sm: 0 }}
             >
-              <Typography
-                variant="h5"
-                component="p"
-                color="#000000"
-                sx={{
-                  display: "flex",
-                  width: "720px",
-                  textAlign: "center",
-                }}
-              >
-                With our Machine Learning algorithm you will get that job in no
-                time. We promise you! Just give us the money and we will take
-                care of it.
+              <Hidden smDown>
+                <Typography variant="h2" component="h1" color="warning">
+                  The
+                </Typography>
+                <Typography variant="h2" component="h1" color="warning">
+                  place
+                </Typography>
+                <Typography variant="h2" component="h1" color="warning">
+                  where
+                </Typography>
+              </Hidden>
+            </Stack>
+
+            <Stack
+              direction={{
+                xs: "column",
+                sm: "row", //600px
+                // md: "row", //900px
+                // lg: "column", // 1200px
+                // xl: "row", //1535px
+              }}
+              spacing={{ xs: 1, sm: 4 }}
+              justifyContent="center"
+              alignItems={"center"}
+            >
+              <Typography variant="h2" component="h1" color="warning">
+                you get
               </Typography>
-            </Box>
+
+              <Typography variant="h2" component="span" color="primary">
+                that
+              </Typography>
+
+              <Typography variant="h2" component="span" color="warning">
+                job
+              </Typography>
+            </Stack>
+
+            <Typography
+              variant="h5"
+              component="p"
+              width={{ xs: 350, sm: 600 }}
+              textAlign="center"
+              marginTop={4}
+            >
+              With our Machine Learning algorithm you will get that job in no
+              time. We promise you! Just give us the money and we will take care
+              of it.
+            </Typography>
+
             <Box
               sx={{
                 display: "flex",
                 justifyContent: "center",
-                paddingTop: "32px",
+                padding: "32px",
               }}
             >
               <Button
@@ -150,114 +145,102 @@ const Homepage = () => {
                 create an account now
               </Button>
             </Box>
+
+            <Stack width={"auto"}>
+              <img src="images/Peoplegroup.svg" alt="woman" width={"auto"} />
+            </Stack>
+          </Stack>
+        </Stack>
+        {/* ----------------------------------------- Article Section ----------------------------------------- */}
+        <Hidden mdDown>
+          <Stack id="Article-section" direction={"row"}>
             <Box
               sx={{
                 display: "flex",
-                justifyContent: "center",
-                paddingTop: "32px",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                width: "55%",
+                backgroundColor: "error.main",
+                padding: "64px 120px",
               }}
             >
-              <img src="images/Peoplegroup.svg" alt="woman" />
+              <Typography
+                variant="h3"
+                component="p"
+                color="primary.contrastText"
+                sx={{
+                  marginBottom: "24px",
+                }}
+              >
+                Find your next job
+              </Typography>
+              <Typography
+                variant="findYouJob"
+                component="p"
+                color="primary.contrastText"
+                sx={{
+                  marginBottom: "40px",
+                  width: "80%",
+                  fontSize: "24px",
+                }}
+              >
+                Our Machine learning algorithm is so good that it’s even illegal
+                in some countries. Join us to use our barelly legal algorithm
+                that is actually a group of interns that work on our basement.
+              </Typography>
+              <Typography
+                variant="findYouJob"
+                component="p"
+                color="primary.contrastText"
+                sx={{
+                  width: "80%",
+                  fontSize: "24px",
+                }}
+              >
+                We have a job for you, no matter your background or previous
+                experience. Is sending random memes through chat your only
+                skill? That’s ok, we got you, our Rock Star Meme Curator role is
+                here for you.
+              </Typography>
             </Box>
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            widows: "auto",
-            display: "flex",
-            flexDirection: "row",
-          }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "flex-start",
-              width: "55%",
-              backgroundColor: "error.main",
-              padding: "64px 120px",
-            }}
-          >
-            <Typography
-              variant="h3"
-              component="p"
-              color="primary.contrastText"
+            <Box
               sx={{
-                marginBottom: "24px",
+                display: "flex",
+                width: "35%",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              Find your next job
-            </Typography>
-            <Typography
-              variant="findYouJob"
-              component="p"
-              color="primary.contrastText"
-              sx={{
-                marginBottom: "40px",
-                width: "80%",
-                fontSize: "24px",
-              }}
-            >
-              Our Machine learning algorithm is so good that it’s even illegal
-              in some countries. Join us to use our barelly legal algorithm that
-              is actually a group of interns that work on our basement.
-            </Typography>
-            <Typography
-              variant="findYouJob"
-              component="p"
-              color="primary.contrastText"
-              sx={{
-                width: "80%",
-                fontSize: "24px",
-              }}
-            >
-              We have a job for you, no matter your background or previous
-              experience. Is sending random memes through chat your only skill?
-              That’s ok, we got you, our Rock Star Meme Curator role is here for
-              you.
-            </Typography>
-          </Box>
-          <Box
-            sx={{
-              display: "flex",
-              width: "35%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <img src="images/loupe.svg" alt="loupe" />
-          </Box>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            width: "auto",
-            height: "490px",
-            backgroundColor: "#E5E5E5",
-          }}
+              <img src="images/loupe.svg" alt="loupe" />
+            </Box>
+          </Stack>
+        </Hidden>
+        {/* ----------------------------------------- Footer Section ----------------------------------------- */}
+        <Stack
+          id="Footer-section"
+          backgroundColor="#E5E5E5"
+          fullWidth
+          direction={"column"}
+          justifyContent="center"
+          alignItems="center"
+          spacing={4}
         >
           <Typography
             variant="h2"
             component="h1"
             color="primary"
-            sx={{
-              display: "flex",
-              marginTop: "140px",
-              justifyContent: "center",
-            }}
+            marginTop={4}
+            fontSize={{ xs: 32, sm: 48 }}
           >
             Meet the team
           </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              marginTop: "24px",
-              gap: "80px",
-            }}
+          <Stack
+            backgroundColor="#E5E5E5"
+            direction={{ xs: "column", sm: "row", md: "row" }}
+            justifyContent="center"
+            alignItems="center"
+            flexWrap={"wrap"}
+            spacing={4}
           >
             {/* Picture your team */}
             {profile.map((profile) => {
@@ -270,43 +253,31 @@ const Homepage = () => {
                 />
               );
             })}
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              marginTop: "75px",
-              backgroundColor: "#E5E5E5",
-            }}
-          >
+          </Stack>
+          <Stack width={"80%"}>
             <Divider
               sx={{
                 display: "flex",
-                width: "80%",
+                width: "100%",
                 borderColor: "#BF5F82",
               }}
             />
-            <Box
-              sx={{
-                width: "100%",
-                flexDirection: "row",
-                justifyContent: "space-around",
-                display: "flex",
-                marginTop: "18px",
-                paddingBottom: "40px",
-              }}
+            <Stack
+              direction={{ xs: "column", sm: "row" }}
+              justifyContent="space-between"
+              alignItems={"center"}
+              spacing={4}
+              textAlign={"center"}
             >
-              <Box>
-                <Typography
-                  variant="underLine"
-                  component="span"
-                  color="warning"
-                >
-                  © 202X - Get That Job
-                </Typography>
-              </Box>
+              <Typography
+                variant="underLine"
+                component="span"
+                color="warning"
+                textAlign={"center"}
+              >
+                © 202X - Get That Job
+              </Typography>
+
               <Box
                 sx={{
                   display: "flex",
@@ -323,6 +294,7 @@ const Homepage = () => {
                     display: "flex",
                     marginBottom: "5px",
                   }}
+                  textAlign={"center"}
                 >
                   Source Code
                 </Typography>
@@ -338,6 +310,7 @@ const Homepage = () => {
                       variant="underLine"
                       component="span"
                       color="warning"
+                      textAlign={"center"}
                       sx={{
                         paddingLeft: "5px",
                         paddingRight: "5px",
@@ -356,6 +329,7 @@ const Homepage = () => {
                         paddingLeft: "5px",
                         paddingRight: "5px",
                       }}
+                      textAlign={"center"}
                     >
                       React Responsive SPA
                     </Typography>
@@ -371,9 +345,9 @@ const Homepage = () => {
                   Codeable - Cohort X Final Project
                 </Typography>
               </Box>
-            </Box>
-          </Box>
-        </Box>
+            </Stack>
+          </Stack>
+        </Stack>
       </Box>
     </>
   );
